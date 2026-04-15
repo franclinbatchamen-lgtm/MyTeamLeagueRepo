23844fbc921affc68a1e1d0b0396b718d4078a02
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-04-15 16:38:22 +0200

    displayed ut on activity main

--- app/src/main/java/com/example/myteamleague/MainActivity.kt
+++ app/src/main/java/com/example/myteamleague/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myteam
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         "Arsenal",
         "Man United")
 
+    private fun arrayof(
+        string: String,
+        string2: String,
+        string3: String,
+        string4: String,
+        string5: String
+    ) {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         val teamsTextView = findViewById<TextView>(R.id.teamsTextView)
 
         teamsTextView.text = teams[0]
+
+        teams[0] = "Real Madrid"
 
 
 
